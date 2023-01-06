a685aec0f8a4283d0bbe42679f02768cef7d12cf
shin katsumaru <s.katsumaru@loglass.co.jp>
2023-01-06 16:01:11 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-                let response = "+PONG\r\n";
-                let mut _stream = _stream;
-                _stream.write(response.as_bytes()).unwrap();
-                _stream.flush().unwrap();
-            }
+            Ok(mut stream) => loop {
+                let mut request_buffer = [0; 512];
+                stream.read(&mut request_buffer).unwrap();
+                let response_string = "+PONG\r\n".as_bytes();
+                stream.write(response_string).unwrap();
+            },
             Err(e) => {
                 println!("error: {}", e);
             }
