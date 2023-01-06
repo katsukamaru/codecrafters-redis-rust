d4bf2bc7dc0c4a597f7d5f59f9999fb9527647da
shin katsumaru <s.katsumaru@loglass.co.jp>
2023-01-06 18:20:27 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use anyhow::Result;
+use bytes::BytesMut;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => loop {
-                let mut request_buffer = [0; 512];
-                stream.read(&mut request_buffer).unwrap();
-                let response_string = "+PONG\r\n".as_bytes();
-                stream.write(response_string).unwrap();
-            },
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let incoming = listener.accept().await;
+        match incoming {
+            Ok((mut stream, _)) => {
+                println!("accepted new connection");
+                tokio::spawn(async move {
+                    handle_connection(&mut stream).await.unwrap();
+                });
+            }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+async fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+    let mut buf = [0; 512];
+
+    loop {
+        // Wait for the client to send us a message but ignore the content for now
+        let bytes_read = stream.read(&mut buf).await?;
+        if bytes_read == 0 {
+            println!("client closed the connection");
+            break;
+        }
+        stream.write("+PONG\r\n".as_bytes()).await?;
+    }
+    Ok(())
 }
