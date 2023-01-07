4abfabb9ef0efc1404d31311aeed964a48b308d0
shin katsumaru <s.katsumaru@loglass.co.jp>
2023-01-07 13:23:54 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,20 @@ use bytes::BytesMut;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
+use resp::Value::{Error, SimpleString};
+mod resp;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut listener = TcpListener::bind("127.0.0.1:6379").await?;
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
     loop {
         let incoming = listener.accept().await;
         match incoming {
-            Ok((mut stream, _)) => {
+            Ok((stream, _)) => {
                 println!("accepted new connection");
                 tokio::spawn(async move {
-                    handle_connection(&mut stream).await.unwrap();
+                    handle_connection(stream).await.unwrap();
                 });
             }
             Err(e) => {
@@ -24,17 +26,26 @@ async fn main() -> Result<()> {
     }
 }
 
-async fn handle_connection(stream: &mut TcpStream) -> Result<()> {
-    let mut buf = [0; 512];
+
+async fn handle_connection(stream: TcpStream) -> Result<()> {
+    let mut conn = resp::RespConnection::new(stream);
 
     loop {
-        // Wait for the client to send us a message but ignore the content for now
-        let bytes_read = stream.read(&mut buf).await?;
-        if bytes_read == 0 {
-            println!("client closed the connection");
+        let value = conn.read_value().await?;
+
+        if let Some(value) = value {
+            let (command, args) = value.to_command()?;
+            let response = match command.to_ascii_lowercase().as_ref() {
+                "ping" => SimpleString("PONG".to_string()),
+                "echo" => args.first().unwrap().clone(),
+                _ => Error(format!("command not implemented: {}", command)),
+            };
+
+            conn.write_value(response).await?;
+        } else {
             break;
         }
-        stream.write("+PONG\r\n".as_bytes()).await?;
     }
+
     Ok(())
 }
